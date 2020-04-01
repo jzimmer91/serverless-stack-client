e1f622eef19671de48eecf7130a0cfa312e37b67
Joe Zimmer <joe.zimmer@edfenergy.com>
2020-04-01 16:54:48 +0100

    new repo

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "notes-app-uploads-bucket-jz"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://h94jwczp59.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_I0il74lNm",
+    APP_CLIENT_ID: "7c1lneths5qkflrff5beke7v4d",
+    IDENTITY_POOL_ID: "eu-west-2:1eabe011-4b2a-41e0-9ca4-3dcbe3b8ee30"
+  }
+};
