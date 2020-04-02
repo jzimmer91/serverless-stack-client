905bc64850e7328216a45c5482fe439b9da1fcdd
Joe Zimmer <joe.zimmer@edfenergy.com>
2020-04-02 12:09:10 +0100

    commiting code before moving

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound"
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route component={NotFound} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute component={NotFound}  appProps={appProps} />
     </Switch>
   );
 }
